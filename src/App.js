8c86fd3b5b8ef3736811c0d689fd122dab4322dc
Gaurav-2812 <gauravkumar21122003@gmail.com>
2023-10-18 20:05:11 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import Content from './components/Content'; // Import the Content component
-
+import { Analytics } from '@vercel/analytics/react';
 const App = () => {
   return (
     <div className="App">
       <Content  heading="Image Gallery"/>
-      
+      <Analytics />
     </div>
   );
 };
